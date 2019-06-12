58d7cb4b3ba12b247eb60a6542f286bb7dd65808
Bojan Gavrovski <bojang@gmail.com>
2019-06-12 22:00:42 +0200

    initial commit

---
+++ c1/index.js
@@ -0,0 +1,26 @@
+console.log('Helloz!');
+
+ime = 'Bojan';
+var prezime = 'Bojanovski';
+let vozrast = 36;
+console.log('kjcslkcjd ' + prezime + ' nclsdkjc');
+console.log(`kjcslkcjd ${prezime} nclsdkjc`);
+
+console.log(ime);
+
+function test() {
+    a = 10;
+    // var b = 20;
+    let c = 30;
+}
+
+test();
+console.log(a);
+a = 5;
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+// + - / % *
+// || && !
+
